be1bcffd195efe54bb4cc452771dbcfb7f26916d
moxonbox <voddl0407@naver.com>
2026-09-22 16:24:45 +0900

    004_Initial:_DefineFunctions

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -1,7 +1,18 @@
 from pico2d import *
 
+
+def move_circle():
+    pass
+
+def move_rectangle():
+    pass
+
+def move_triangle():
+    pass
+
+
 while(1):
-    
+
     move_circle()
     move_rectangle()
     move_triangle()
